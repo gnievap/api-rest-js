4bcd16537ad15f2a03da0f84035ce1bae8d710bb
Gaby Nieva <gaby.nieva@outlook.com>
2023-08-26 23:58:52 -0600

    Adding CSS

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ console.log("Hello, world");
 
 const URL = 'https://api.thedogapi.com/v1/images/search';
 
-fetch(URL)
-   .then(res => res.json())
-   .then(data => {
-      const img = document.querySelector('img');
-      img.src = data[0].url;
-   });
+// fetch(URL)
+//    .then(res => res.json())
+//    .then(data => {
+//       const img = document.querySelector('img');
+//       img.src = data[0].url;
+//    });
+
+   const button = document.querySelector("button");
+   
+   button.onclick = fetch(URL);
