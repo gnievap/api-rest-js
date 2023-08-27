ec231dad624867f2100fd8c005f59da863e2495a
Gaby Nieva <gaby.nieva@outlook.com>
2023-08-27 00:06:02 -0600

    Trying async function to retrieve another dog --- doesn't works

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("Hello, world");
+//console.log("Hello, world");
 
 const URL = 'https://api.thedogapi.com/v1/images/search';
 
@@ -9,6 +9,17 @@ const URL = 'https://api.thedogapi.com/v1/images/search';
 //       img.src = data[0].url;
 //    });
 
+   const randomDog = async(url) => {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        const img = document.querySelector('img');
+        img.src = data[0].url;
+    } catch (error){
+        console.error(error);
+    }
+   }
+
    const button = document.querySelector("button");
    
-   button.onclick = fetch(URL);
+   button.onclick = randomDog(URL);
