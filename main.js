9d01bd7033a62e3ac72edb05d3fd9833ffa8d1de
Gaby Nieva <gaby.nieva@outlook.com>
2023-08-28 23:17:15 -0600

    Endpoints & query parameters

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //console.log("Hello, world");
 
-const API_URL = 'https://api.thedogapi.com/v1/images/search';
+const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3';
 
 
 //    const randomDog = async(url) => {
@@ -18,8 +18,15 @@ const API_URL = 'https://api.thedogapi.com/v1/images/search';
         const res = await fetch(API_URL);
         const data = await res.json();
 
-        const img = document.querySelector('img');
-        img.src = data[0].url;
+        console.log(data);
+        const img1 = document.getElementById('img1');
+        const img2 = document.getElementById('img2');
+        const img3 = document.getElementById('img3');
+        //img.src = data[0].url;
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+        img3.src = data[2].url;
+
     }
 
     randomDog();
