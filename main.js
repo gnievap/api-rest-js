2ee54601b9a6e74b0f9a1580fdad4350979ef4a2
Gaby Nieva <gaby.nieva@outlook.com>
2023-09-06 21:49:47 -0600

    GET: getting fav doggies and preparing for error codes

--- main.js
+++ main.js
@@ -1,36 +1,43 @@
-//console.log("Hello, world");
-
-const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3&api_key=live_6OcbOSDEuvByGMgT9krNEMu1FdVY4ASuJ55FpMeg2ytAAA2KWc5tiWVFK89J4ToP   ';
 
 
-//    const randomDog = async(url) => {
-//     try {
-//         const res = await fetch(url);
-//         const data = await res.json();
-//         const img = document.querySelector('img');
-//         img.src = data[0].url;
-//     } catch (error){
-//         console.error(error);
-//     }
-//    }
+const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=2&api_key=live_6OcbOSDEuvByGMgT9krNEMu1FdVY4ASuJ55FpMeg2ytAAA2KWc5tiWVFK89J4ToP   ';
+const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites?limit=2&api_key=live_6OcbOSDEuvByGMgT9krNEMu1FdVY4ASuJ55FpMeg2ytAAA2KWc5tiWVFK89J4ToP   ';
+const spanError = document.getElementById('error');
 
-    async function randomDog() {
-        const res = await fetch(API_URL);
-        const data = await res.json();
+async function loadRandomDogs() {
+    const res = await fetch(API_URL_RANDOM);
+    const data = await res.json();
 
-        console.log(data);
+    console.log('Random');
+    console.log(data);
+
+    if ( res.status !== 200 ){
+        spanError.innerHTML = "Hubo un error: " + res.status;
+    } else {
         const img1 = document.getElementById('img1');
         const img2 = document.getElementById('img2');
-        const img3 = document.getElementById('img3');
-        //img.src = data[0].url;
+
         img1.src = data[0].url;
         img2.src = data[1].url;
-        img3.src = data[2].url;
+    }
+    
 
+}
+
+async function loadFavoritesDogs() {
+    const res = await fetch(API_URL_FAVORITES);
+    const data = await res.json();
+
+    console.log('Favs');
+    console.log(data);
+
+    if ( res.status !== 200 ){
+        spanError.innerHTML = "Hubo un error: " + res.status + " " + data.message;
     }
 
-    randomDog();
+}
 
-   //const button = document.querySelector("button");
-   
-   //button.onclick = randomDog(URL);
+
+
+loadRandomDogs();
+loadFavoritesDogs();    
